0ccd4de98d9f432053552edde65691888b564fdf
Leo Tenenbaum <pommicket@gmail.com>
2021-03-06 16:40:07 -0500

    don't reload file when opened while already open

--- ted.c
+++ ted.c
@@ -234,7 +234,6 @@ static bool ted_open_file(Ted *ted, char const *filename) {
 	for (u16 i = 0; i < TED_MAX_BUFFERS; ++i) {
 		if (buffers_used[i]) {
 			if (buffers[i].filename && paths_eq(path, buffers[i].filename)) {
-				buffer_reload(&buffers[i]); // make sure buffer is up to date with the file
 				ted_switch_to_buffer(ted, &buffers[i]);
 				return true;
 			}
