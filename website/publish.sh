9e1dfd028eb899496eb6e91dc2f91618e6be04d5
pommicket <pommicket@gmail.com>
2026-08-02 10:19:05 -0400

    Fix website publish script

--- website/publish.sh
+++ website/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd $(dirname $0) || exit 1
 cargo run -- --www || exit 1
-rclone copy -P dist linode-br:/ted.pommicket.com/ || exit 1
+rclone copy -P dist linode-fr:/ted.pommicket.com/ || exit 1
